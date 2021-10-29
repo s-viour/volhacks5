db74a8c3da248c8665bb35143017af90b64fad06
Matt Wyatt <mwyatt1000@gmail.com>
2021-10-29 19:18:33 -0400

    initial project setup

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+require('dotenv').config()
+
+
+app.get('/', (req, res) => {
+  res.send('hiiiiii!!');
+});
+
+app.listen(process.env.port, () => {
+  console.log(`app listening on localhost:${process.env.port}`);
+});
